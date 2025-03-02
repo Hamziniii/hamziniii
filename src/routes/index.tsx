3f88c54238f418717c5e9ac17e66bb2591b7daeb
Hamza G. <hamzagaziuddin04@gmail.com>
2025-03-02 13:17:46 -0600

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main class="text-center mx-auto text-amber-300 p-4 h-screen flex flex-col items-center justify-center">
+      <h1 class="text-9xl grow place-content-center">coming soon! - hamza</h1>
+    </main>
+  );
+}
